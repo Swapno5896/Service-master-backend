90c2989bb19c9a8bc709dc5d40311009aa52dd29
swapno5896 <swapno5896@yahoo.com>
2021-07-09 14:35:32 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -33,30 +33,24 @@ client.connect((err) => {
 
 
 
-  // post multiple blogs
+  // post single blogs
   app.post("/addBlog", (req, res) => {
-    const fakeBlog = req.body;
-    // console.log(fakeBlog);
-    blogCollection.insertOne(fakeBlog).then((result) => {
+    blogCollection.insertOne(req.body).then((result) => {
       console.log(result.insertedCount);
     });
   });
 
-  // post multiple Portfolio
+  // post single Portfolio
   app.post("/addPortfolio", (req, res) => {
-    const fakeBlog = req.body;
-    // console.log(fakeBlog);
-    portfoliosCollection.insertOne(fakeBlog).then((result) => {
+    portfoliosCollection.insertOne(req.body).then((result) => {
       console.log(result.insertedCount);
     });
   });
 
 
-  // post multiple products
+  // post single products
   app.post("/addProducts", (req, res) => {
-    const fakeBlog = req.body;
-    // console.log(fakeBlog);
-    productsCollection.insertOne(fakeBlog).then((result) => {
+    productsCollection.insertOne(req.body).then((result) => {
       console.log(result.insertedCount);
     });
   });
@@ -86,7 +80,6 @@ client.connect((err) => {
 
 
   // get spacafic blog
-
   const ObjId = require('mongodb').ObjectID
   app.get('/getBlog/:id', (req, res) => {
     blogCollection.find({ _id: ObjId(req.params.id) })
@@ -95,10 +88,16 @@ client.connect((err) => {
       })
   })
 
+  // get specific portfolio
+  app.get('/getProduct/:id', (req, res) => {
+    portfoliosCollection.find({ _id: ObjId(req.params.id) })
+      .toArray((err, doc) => {
+        res.send(doc)
+      })
+  })
 
 
   // get spacafic Product
-
   // const ObjId = require('mongodb').ObjectID
   app.get('/getProduct/:id', (req, res) => {
     productsCollection.find({ _id: ObjId(req.params.id) })
@@ -123,75 +122,7 @@ client.connect((err) => {
 
 
 
-  // 
-  //   add some data
-  // app.post("/addBlog", (req, res) => {
-  //   const file = req.body.myFile.name;
-  //   console.log(file);
-  //   // file.mv(`${__dirname}/doctors/${file.name}`, (err) => {
-  //   //   console.log(err);
-  //   // });
-  //   // // console.log(req.body.myFile);
-  //   // blogCollection.insertOne(fakeBlog).then((result) => {
-  //   //   console.log(result.insertedCount);
-  //   // });
-  // });
 
-  // get some data
-
-  // app.get("/getBlog", (req, res) => {
-  //   blogCollection.find({}).toArray((err, document) => {
-  //     res.send(document);
-  //   });
-  // });
-
-  // delete
-
-  // const ObjectId = require("mongodb").ObjectId;
-
-  // app.delete("/deletBlog/:id", (req, res) => {
-  //   // console.log(req.params.id);
-  //   blogCollection
-  //     .deleteOne({ _id: ObjectId(req.params.id) })
-  //     .then((result) => console.log(result));
-  // });
-
-  // const ObjectId = require("mongodb").ObjectId; age kora hyca tai akn kora lagbe na
-
-  // app.delete("/deleteData/:id", (req, res) => {
-  //   console.log(req.params.id);
-  //   dataCollection
-  //     .deleteOne({ _id: ObjectId(req.params.id) })
-  //     .then((result) => console.log(result));
-  // });
-
-  // app.get("/blogs/:id", (req, res) => {
-  //   blogCollection
-  //     .find({ _id: ObjectId(req.params.id) })
-  //     .toArray((err, document) => {
-  //       res.send(document);
-  //     });
-  // });
-
-  // here we'll update our data
-  // app.patch("/update", (req, res) => {
-  //   // console.log(req.params.id);
-  //   // console.log(req.params.id,req.body.status);
-  //   console.log("four", req.body.status, req.body.id);
-  //   // const ObjectId = require("mongodb").ObjectId;
-  //   bookedServiceCollection
-  //     .updateOne(
-  //       { _id: ObjectId(req.body.id) },
-  //       {
-  //         $set: { status: req.body.status },
-  //       }
-  //     )
-
-  //     .then((result) => {
-  //       console.log("three", result);
-  //       res.send(result);
-  //     });
-  // });
 });
 
 // jkdslklasdlk
